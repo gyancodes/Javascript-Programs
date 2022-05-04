cf28258706b374e5405e16ee377fa82a89d006c5
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-05-04 18:40:07 +0530

    array

---
+++ array.js
@@ -0,0 +1,7 @@
+let array1 = [10,20,30,40,50];
+let array2 = [60,70,80,90,100];
+let car = ["Tata","Mahindra","Tesla","Bugatti","Ferrari"]
+
+let arrayResult = array1.concat(array2,car);
+
+console.log(arrayResult)
