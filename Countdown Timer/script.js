a5638866774ee3123250c086e23bd88bfd14685a
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-10-16 14:14:45 +0530

    update

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -1,21 +1,35 @@
+const d = document.getElementById('days');
+const h = document.getElementById('hours');
+const m = document.getElementById('mins');
+const s = document.getElementById('seconds');
+
 const newYears = '1 Jan 2023';
 
-function countdown(){
-    const newYearsData= new Date(newYears);
+function countdown() {
+    const newYearsData = new Date(newYears);
     const currentDate = new Date();
 
     const totalSeconds = (newYearsData - currentDate) / 1000
 
-    const days = Math.floor(totalSeconds / 3600 /24);
+    const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600 % 24);
-    const minutes = Math.floor(totalSeconds / 60 % 60);
+    const mins = Math.floor(totalSeconds / 60 % 60);
     const seconds = Math.floor(totalSeconds % 60);
-    
 
-    console.log(totalSeconds);
- 
+
+    d.innerHTML = days;
+    h.innerHTML = formatTime(hours);
+    m.innerHTML = formatTime(mins);
+    s.innerHTML = formatTime(seconds);
+
+    // console.log(days, hours, minutes, seconds);
 }
 
-countdown();
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+    
+    
+}
 
-
+countdown()
+setInterval(countdown, 1000)
