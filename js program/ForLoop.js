3547568f9a61aa2486e4c66a67451ea9ad1e4737
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-10-17 22:46:19 +0530

    add

--- js program/ForLoop.js
+++ js program/ForLoop.js
@@ -16,9 +16,9 @@
 
 ;
 
-for(let i=5;i<51; i =i+5){
-    console.log(i)
-}
+// for(let i=5;i<51; i =i+5){
+//     console.log(i)
+// }
 
 // for(let i=50; i>4; i =i-5){
 //     console.log(i);
@@ -29,3 +29,27 @@ for(let i=5;i<51; i =i+5){
 //         console.log(i);
 //     }
 // }
+
+// for (let i = 0; i < 10^10; i++) {
+//     console.log(i);
+    
+// }
+
+function myFunc(theObject) {
+    theObject.make = 'Toyota';
+  }
+  
+  const mycar = {
+    make: 'Honda',
+    model: 'Accord',
+    year: 1998,
+  };
+  
+  // x gets the value "Honda"
+  const x = mycar.make;
+  
+  // the make property is changed by the function
+  myFunc(mycar);
+  // y gets the value "Toyota"
+  const y = mycar.make;
+  
