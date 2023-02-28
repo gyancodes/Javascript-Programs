7704388384fc2fe0772d5391f01950ba61d84bb6
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-02-28 23:41:32 +0530

    arrayalignment program

---
+++ Algorithms/ArrayAlignment.js
@@ -0,0 +1,16 @@
+const permute = function(nums) {
+    let results = [];
+let go = (current) => {
+      if (current.length === nums.length){
+        results.push(current);
+        return;
+      }
+      nums.forEach(n => {
+        if (!current.includes(n)){
+          go([...current, n]);
+        }
+      });
+    }
+    go([]);
+    return results;
+};
