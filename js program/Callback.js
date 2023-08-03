548e719d7827658f940d85240d6e3deef25638c6
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-08-04 00:43:36 +0530

    100DaysOfReact-Day1

--- js program/Callback.js
+++ js program/Callback.js
@@ -1,8 +1,15 @@
-function add(num1,num2){
-    var sum = num1 + num2
-    return sum
-    
+
+// Callback function is a function that is passed to another function as a parameter and the callback function is called inside the function.
+
+// code for callback function
+
+function doSomethingAsync(callback){
+    setTimeout(function(){
+        callback('done');
+    }, 3000);
+
 }
 
-
-console.log(add(2,2))
+doSomethingAsync(function(result){
+    console.log(result);
+})
