8ebd2ed0c33238de73360d14528e4a95f7683675
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-11-06 23:57:54 +0530

    .

---
+++ React/App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading", title: "This is a title." },
+  "My First React Element"
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
