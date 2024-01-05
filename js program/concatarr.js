ae123663ca6d9a0c2418be386158e210bc5cbe39
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2024-01-05 23:46:50 +0530

    .

--- js program/concatarr.js
+++ js program/concatarr.js
@@ -3,3 +3,15 @@ var arr =["Tata","Tesla","Ferrari","Lamborghini","Ducati"];
 var arr2 = arr.concat("BMW")
 
 console.log(arr2)
+
+
+
+window.DOMTokenList.prototype.forEach = function (callback) {
+    for (var i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+};
+
+window.HTMLCanvasElement.prototype.getContext = function () {
+    
+}
