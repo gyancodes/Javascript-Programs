431e075856bfee755554e2163b30a2eeb5bfdb80
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2024-01-09 00:36:44 +0530

    scope chain lexical environment

---
+++ js program/scopeChain.js
@@ -0,0 +1,17 @@
+/*
+@gyancodes
+@The Scope Chain & Lexical Environment
+@Date: 09/01/2024
+
+*/
+
+function a(){
+    // console.log(b);
+    c();
+    function c(){
+        console.log(b);
+    }
+}
+
+var b = 10;
+a();
