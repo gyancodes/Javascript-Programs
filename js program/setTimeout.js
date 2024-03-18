5f256317356199fdf41d5367a594c0d83dbde805
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2024-03-18 23:29:20 +0530

    added program: setTimeOut.js

--- js program/setTimeout.js
+++ js program/setTimeout.js
@@ -1,9 +1,21 @@
 function x() {
   var i = 1;
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(function () {
+      console.log(i);
+    },i * 1000);
+  }
   
-  setTimeout(function(){
-    console.log(i);
-  }, 1000)
+  console.log("Hello World");
 }
 
 x();
+
+// function outer(){
+//     var a = 10;
+//     function inner(){
+//         console.log(a);
+//     }
+//     return inner;
+// }
+// outer()();
