ea2be2d0850d616c75018cc50ea597258d643832
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2024-09-23 10:24:36 +0530

    array

--- js program/array.js
+++ js program/array.js
@@ -4,17 +4,17 @@ let car = ["Tata","Mahindra","Tesla","Bugatti","Ferrari"]
 
 let arrayResult = array1.concat(array2,car);
 
-const myArr2 = new Array(1, 2,  3, 4)
+// const myArr2 = new Array(1, 2,  3, 4)
 
 // console.log(myArr2)
 
 
 // Array methods
 
-myArr2.push(6)
-myArr2.push(10)
+// myArr2.push(6)
+// myArr2.push(10)
 // myArr2.pop()
-console.log(myArr2);
+// console.log(myArr2);
 
 // array
 
@@ -55,3 +55,35 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
 console.log(myn2);
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
