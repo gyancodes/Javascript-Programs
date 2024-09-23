51938b691aa914b08eb2f34f3ed4cf848adc3719
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2024-09-23 13:16:42 +0530

    function

--- js program/function.js
+++ js program/function.js
@@ -4,3 +4,42 @@ function greet(){
 
 
 greet();
+
+
+function sayMyName(){
+    console.log("G");
+    console.log("Y");
+    console.log("A");
+    console.log("N");
+    
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("gyan"))
+// console.log(loginUserMessage("gyan"))
